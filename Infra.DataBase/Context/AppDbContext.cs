27cb1c7d1ad1afd0f36a80c92b6cb60d219ecfdb
jorgel333 <jorgeleonardo333@gmail.com>
2023-10-02 22:28:14 -0300

    Criando Projeto do modelo de dominio

---
+++ Infra.DataBase/Context/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Infra.DataBase.Context;
+
+public class AppDbContext : DbContext
+{
+	public AppDbContext(DbContextOptions<AppDbContext> opt) : base(opt)
+	{
+		
+	}
+    public DbSet<Admin> Administrators { get; set; }
+    public DbSet<User> CommonUsers { get; set; }
+    public DbSet<Movie> Movies { get; set; }
+    public DbSet<AssessmentRecord> AssessmentRecords { get; set; }
+    public DbSet<Casts> Casts { get; set; }
+    public DbSet<CastActMovies> CastActMovies { get; set; }
+    public DbSet<CastDirectMovies> CastDirectMovies { get; set; }
+    public DbSet<Genre> Genres { get; set; }
+}
